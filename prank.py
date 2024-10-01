dbbcff4c65e313e611dd9d8eee81b472d21359ec
KS Ankith <88841820+kscypher@users.noreply.github.com>
2024-10-01 18:48:29 +0530

    Create prank.py

---
+++ prank.py
@@ -0,0 +1,14 @@
+import random
+
+import pyautogui as pg
+
+import time
+
+animal = ('monkey','donkey','chimpanzee')
+
+time.sleep(😎
+
+for i in range(500):
+    a=random.choice(animal)
+    pg.write("Rey "+a)
+    pg.press('enter')
